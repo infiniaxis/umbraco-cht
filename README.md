41c70d9e567b2f2e829c7100633e27fc3c5e6945
infiniaxis <infiniaxis@wellace.org>
2016-10-05 16:50:50 +0800

    add readme, modify licence font size

---
+++ README.md
@@ -0,0 +1,8 @@
+<a href="https://readthedocs.org/projects/umbraco-cht/builds/"><img src="https://readthedocs.org/projects/umbraco-cht/badge/?version=latest"></a>
+
+
+## Umbraco 中文介紹說明
+
+閱讀文件請至 http://umbraco-cht.rtfd.io/
+
+對應 Umbraco 版本 7.5.x
--- conf.py
+++ conf.py
@@ -38,34 +38,6 @@ else:				# only import and set the theme if we're building docs locally
 	html_theme = 'sphinx_rtd_theme'
 	html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-latex_elements = {
-		# The paper size ('letterpaper' or 'a4paper').
-		#'papersize': 'letterpaper',
-
-		# The font size ('10pt', '11pt' or '12pt').
-		#'pointsize': '10pt',
-
-		# Additional stuff for the LaTeX preamble.
-		#'preamble': '',
-		'preamble': r'''
-		\hypersetup{unicode=true}
-		\usepackage{CJKutf8}
-		\DeclareUnicodeCharacter{00A0}{\nobreakspace}
-		\DeclareUnicodeCharacter{2203}{\ensuremath{\exists}}
-		\DeclareUnicodeCharacter{2200}{\ensuremath{\forall}}
-		\DeclareUnicodeCharacter{2286}{\ensuremath{\subseteq}}
-		\DeclareUnicodeCharacter{2713}{x}
-		\DeclareUnicodeCharacter{27FA}{\ensuremath{\Longleftrightarrow}}
-		\DeclareUnicodeCharacter{221A}{\ensuremath{\sqrt{}}}
-		\DeclareUnicodeCharacter{221B}{\ensuremath{\sqrt[3]{}}}
-		\DeclareUnicodeCharacter{2295}{\ensuremath{\oplus}}
-		\DeclareUnicodeCharacter{2297}{\ensuremath{\otimes}}
-		\begin{CJK}{UTF8}{gbsn}
-		\AtEndDocument{\end{CJK}}
-		''',
-}
-
-
 # If your documentation needs a minimal Sphinx version, state it here.
 #
 # needs_sphinx = '1.0'
