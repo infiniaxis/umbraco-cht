79742beef4bc88b3f41e14eabe4134a1912ea291
infiniaxis <infiniaxis@wellace.org>
2016-10-05 15:32:43 +0800

    chinese setting

--- conf.py
+++ conf.py
@@ -31,7 +31,33 @@ if on_rtd: 			# Various settings to pass to templates:
     html_context = { 
     	"google_analytics_id" : 'UA-85199427-2', 
     	"disqus_shortname" : 'umbraco-cht' 
-    	}
+    }
+    latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #'papersize': 'letterpaper',
+
+    # The font size ('10pt', '11pt' or '12pt').
+    #'pointsize': '10pt',
+
+    # Additional stuff for the LaTeX preamble.
+    'babel'     : r'''\usepackage[chinese]{babel}''',
+    'preamble': r'''
+    \hypersetup{unicode=true}
+    \usepackage{CJKutf8}
+    \DeclareUnicodeCharacter{00A0}{\nobreakspace}
+    \DeclareUnicodeCharacter{2203}{\ensuremath{\exists}}
+    \DeclareUnicodeCharacter{2200}{\ensuremath{\forall}}
+    \DeclareUnicodeCharacter{2286}{\ensuremath{\subseteq}}
+    \DeclareUnicodeCharacter{2713}{x}
+    \DeclareUnicodeCharacter{27FA}{\ensuremath{\Longleftrightarrow}}
+    \DeclareUnicodeCharacter{221A}{\ensuremath{\sqrt{}}}
+    \DeclareUnicodeCharacter{221B}{\ensuremath{\sqrt[3]{}}}
+    \DeclareUnicodeCharacter{2295}{\ensuremath{\oplus}}
+    \DeclareUnicodeCharacter{2297}{\ensuremath{\otimes}}
+    \begin{CJK}{UTF8}{gbsn}
+    \AtEndDocument{\end{CJK}}
+    ''',
+    }
 else:				# only import and set the theme if we're building docs locally
 	import sphinx_rtd_theme
 	html_theme = 'sphinx_rtd_theme'
